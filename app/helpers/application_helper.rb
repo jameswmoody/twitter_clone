984046be530372e3ce53da2835545588f941b30e
James W Moody <jamesmoodyradio@gmail.com>
2017-02-01 22:40:19 -0600

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "Twitter (But for Dogs)"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
